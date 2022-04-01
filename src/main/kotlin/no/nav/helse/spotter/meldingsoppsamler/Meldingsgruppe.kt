2167a6cfbdbf519a86a4353606449215be50434e
Erik M.Forsman <erik.maximilian.forsman@nav.no>
2022-04-01 13:17:39 +0200

    Prøver å sjekke på ider på enklere måte

--- src/main/kotlin/no/nav/helse/spotter/meldingsoppsamler/Meldingsgruppe.kt
+++ src/main/kotlin/no/nav/helse/spotter/meldingsoppsamler/Meldingsgruppe.kt
@@ -1,19 +1,21 @@
 package no.nav.helse.spotter.meldingsoppsamler
 
 import java.time.LocalDateTime
+import java.util.UUID
 
 internal class Meldingsgruppe(melding: Melding) {
     private val meldinger = mutableListOf(melding)
+    private val ider = mutableSetOf<UUID>().also { it.addAll(melding.ider) }
 
     internal fun leggTil(melding: Melding): Meldingsgruppe? {
-        if (meldinger.none { it.kjennerTil(melding) }) return null
+        if (ider.intersect(melding.ider).isEmpty()) return null
         meldinger.add(melding)
+        ider.addAll(melding.ider)
         return this
     }
 
     internal fun meldinger() = meldinger.sortedBy { it.deltaker.tidspunkt }
     internal fun oppdatertEtter(tidspunkt: LocalDateTime) = meldinger.any { it.oppdatertEtter(tidspunkt) }
 
-    private fun Melding.kjennerTil(melding: Melding) = this.ider.intersect(melding.ider).isNotEmpty()
     private fun Melding.oppdatertEtter(tidspunkt: LocalDateTime) = this.deltaker.tidspunkt > tidspunkt
 }
